31dd620ddb9b85832c587b5d60fddca88b26250a
AWr1ght <allister.wwight@gmail.com>
2013-02-02 12:04:22 -0600

    Just cleaning up some weird errors

---
+++ src/org/usfirst/frc3946/UltimateAscent/commands/ArmForward.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc3946.UltimateAscent.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc3946.UltimateAscent.Robot;
+
+/**
+ *
+ * @author AJ
+ */
+public class ArmForward extends Command {
+    
+    public ArmForward() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+//        requires(Robot.binaryLoadLifter);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+//        loadLifter.forward();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+//        loadLifter.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        end();
+    }
+}
---
+++ src/org/usfirst/frc3946/UltimateAscent/subsystems/FirePiston.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc3946.UltimateAscent.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author AJ
+ */
+public class FirePiston extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    
+    public void extend(){
+        
+    }
+    
+    public void retract(){
+        
+    }
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
