6cca857aa01668614554476262243424c6eb9bf3
gixxy <gus3@michelfamily.org>
2013-02-17 09:21:56 -0600

    Camera Work.

---
+++ src/org/usfirst/frc3946/UltimateAscent/subsystems/RaspberryPi.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc3946.UltimateAscent.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc3946.UltimateAscent.SocketPi;
+import org.usfirst.frc3946.UltimateAscent.commands.PrintPiData;
+
+/**
+ *
+ * @author Gustave Michel
+ */
+public class RaspberryPi extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    SocketPi raspberryPi;
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        //setDefaultCommand(new PrintPiData());
+    }
+    public SocketPi getPi() {
+        return raspberryPi;
+    }
+    
+    public RaspberryPi() {
+        super();
+        //raspberryPi = new SocketPi();
+        System.out.println(this.getClass().getName()+" Initialized");
+    }
+}
