2dfb1a18a76c147c1f746968af2f365077776e88
AWr1ght <allister.wwight@gmail.com>
2013-03-21 21:55:31 -0500

    Properly adding user friendly files

---
+++ src/org/usfirst/frc3946/UltimateAscent/commands/KWindageLeft.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc3946.UltimateAscent.commands;
+
+/**
+ *
+ * @author AJ
+ */
+public class KWindageLeft extends CommandBase {
+    
+    public KWindageLeft() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        AutoAim.aimLeft();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
